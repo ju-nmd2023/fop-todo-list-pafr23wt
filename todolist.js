ce386621d046015adb21ebe68168bbe090fcfb60
pafr23wt <148805767+pafr23wt@users.noreply.github.com>
2024-04-17 13:42:44 +0200

    upd

--- todolist.js
+++ todolist.js
@@ -1,13 +1,1 @@
 /** @format */
-
-const headlineElement = document.querySelector("h1");
-console.log(headlineElement);
-
-const tasks = ["buy milk", "clean", "wash"];
-const taskListElement = document.getElementById("taskList");
-
-for (let task of tasks) {
-  const taskElement = document.createElement("div");
-  taskElement.innerText = task;
-  taskListElement.appendChild(taskElement);
-}
