1b5ce3f02af3f5a548579d9b4aa1e5c77b1619fe
pafr23wt <148805767+pafr23wt@users.noreply.github.com>
2024-04-17 22:33:02 +0200

    upd

--- todolist.js
+++ todolist.js
@@ -19,3 +19,27 @@ const saveButton = document.getElementById("save");
 saveButton.addEventListener("click", function () {
   saveTasks();
 });
+
+function displayTasks() {
+  const tasksElement = document.getElementById("tasks");
+  tasksElement.innerHTML = "";
+
+  if (localStorage.tasks !== undefined) {
+    let tasksArray = JSON.parse(localStorage.tasks);
+
+    for (let task of tasksArray) {
+      const item = document.createElement("li");
+      item.innerText = task.name;
+
+      //inspo by ChatGPT
+      // Create checkbox
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.addEventListener("change", function () {
+        if (checkbox.checked) {
+          item.classList.add("completed");
+        } else {
+          item.classList.remove("completed");
+        }
+      });
+      item.appendChild(checkbox);
